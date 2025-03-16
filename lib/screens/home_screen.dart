04eea767a8fec247695b22c500b5bed484450758
Shiva Pal <shivapal7504@gmail.com>
2025-03-16 12:49:54 +0530

    added delete method

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,10 @@
-import 'package:authentication_app/screens/form_screen.dart';
 import 'package:authentication_app/screens/profile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/data_provider.dart';
 import '../providers/user_provider.dart';
 import 'detail_screen.dart';
+import 'form_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,15 +15,14 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
-  void initState() {
-    super.initState();
+  void didChangeDependencies() {
+    super.didChangeDependencies();
     Provider.of<DataProvider>(context, listen: false).fetchData();
   }
 
   @override
   Widget build(BuildContext context) {
     final userProvider = Provider.of<UserProvider>(context);
-    final dataProvider = Provider.of<DataProvider>(context);
     final user = userProvider.user;
 
     return Scaffold(
@@ -44,29 +43,34 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
-      body: ListView.builder(
-          itemCount: dataProvider.dataList.length,
-          itemBuilder: (context, index) {
-            final data = dataProvider.dataList[index];
-            return ListTile(
-              title: Text(data.title),
-              subtitle: Text(data.category),
-              // trailing: Text(DateFormat.yMMMd().format(data.date)),
-              onTap: () {
-                dataProvider.setSelectedItem(data);
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const DetailScreen(),
-                  ),
-                );
-              },
-              trailing: IconButton(
-                icon: const Icon(Icons.delete),
-                onPressed: () => dataProvider.deleteData(data.id),
-              ),
-            );
-          }),
+      body: Consumer<DataProvider>(
+        builder: (context, dataProvider, child) {
+          if (dataProvider.dataList.isEmpty) {
+            return const Center(child: Text("No items available"));
+          }
+
+          return ListView.builder(
+            itemCount: dataProvider.dataList.length,
+            itemBuilder: (context, index) {
+              final data = dataProvider.dataList[index];
+              return ListTile(
+                title: Text(data.title),
+                subtitle: Text("Category: ${data.category}"),
+                trailing: IconButton(onPressed: (){
+                  _deleteItem(context, data.id);
+                }, icon: const Icon(Icons.delete)),
+                onTap: () {
+                  dataProvider.setSelectedItem(data);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const DetailScreen()),
+                  );
+                },
+              );
+            },
+          );
+        },
+      ),
       floatingActionButton: FloatingActionButton(
           onPressed: () {
             Navigator.push(
@@ -74,5 +78,11 @@ class _HomeScreenState extends State<HomeScreen> {
           },
           child: const Icon(Icons.add)),
     );
+  }
+
+  void _deleteItem(BuildContext context, String id) {
+    final dataProvider = Provider.of<DataProvider>(context, listen: false);
+    dataProvider.deleteData(id);
+    // Navigator.pop(context);
   }
 }
