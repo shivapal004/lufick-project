c4fac55a4164da1b167b360e1dc85a440eea3cd7
Shiva pal <142173540+shivapal004@users.noreply.github.com>
2025-03-31 11:22:09 +0530

    commited

--- lib/models/student_model.dart
+++ lib/models/student_model.dart
@@ -4,7 +4,6 @@ class StudentModel {
   String email;
   String phone;
   String gender;
-  // List<String> enrolledCourseIds;
 
   StudentModel({
     required this.id,
@@ -12,26 +11,5 @@ class StudentModel {
     required this.email,
     required this.phone,
     required this.gender
-    // required this.enrolledCourseIds,
   });
-
-  // factory StudentModel.fromMap(Map<String, dynamic> map, String id) {
-  //   return StudentModel(
-  //     id: id,
-  //     name: map['name'] ?? '',
-  //     email: map['email'] ?? '',
-  //     phone: map['phone'] ?? '',
-  //     enrolledCourseIds: List<String>.from(map['enrolled_course_ids'] ?? []),
-  //   );
-  // }
-  //
-  // Map<String, dynamic> toMap() {
-  //   return {
-  //     'id': id,
-  //     'name': name,
-  //     'email': email,
-  //     'phone': phone,
-  //     'enrolled_course_ids': enrolledCourseIds,
-  //   };
-  // }
 }
