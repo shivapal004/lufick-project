53ad7199ab2936da7da9f59782d7f8e1da288d9a
Shiva pal <142173540+shivapal004@users.noreply.github.com>
2025-03-31 11:22:59 +0530

    commited

--- lib/models/enrollment_model.dart
+++ lib/models/enrollment_model.dart
@@ -5,32 +5,11 @@ class EnrollmentModel {
   String studentId;
   String courseId;
   DateTime enrollmentDate;
-  // String status;
 
   EnrollmentModel({
     required this.id,
     required this.studentId,
     required this.courseId,
     required this.enrollmentDate,
-    // required this.status,
   });
-
-  // factory EnrollmentModel.fromMap(Map<String, dynamic> map, String id) {
-  //   return EnrollmentModel(
-  //     id: id,
-  //     studentId: map['studentId'] ?? '',
-  //     courseId: map['courseId'] ?? '',
-  //     enrollmentDate: DateTime.parse(map['enrollmentDate']),
-  //     status: map['status'] ?? 'pending',
-  //   );
-  // }
-  //
-  // Map<String, dynamic> toMap() {
-  //   return {
-  //     'studentId': studentId,
-  //     'courseId': courseId,
-  //     'enrollmentDate': enrollmentDate.toIso8601String(),
-  //     'status': status,
-  //   };
-  // }
 }
