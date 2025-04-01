3eeb303870ec17af58ca349415171c796ac34fac
shiva pal <shivapal7504@gmail.com>
2025-04-01 12:35:46 +0530

    (INCOMPLETE) bmi feature
    add elevated button
    updated new button

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:authentication_app/screens/BMI%20Calculator/bmi_home_page.dart';
 import 'package:authentication_app/screens/profile_screen.dart';
 import 'package:authentication_app/screens/task_one/main_screen.dart';
 import 'package:authentication_app/screens/task_two/dashboard_screen.dart';
@@ -28,8 +29,7 @@ class _HomeScreenState extends State<HomeScreen> {
           Provider.of<CourseProvider>(context, listen: false);
       final enrollmentProvider =
           Provider.of<EnrollmentProvider>(context, listen: false);
-      final dataProvider =
-      Provider.of<DataProvider>(context, listen: false);
+      final dataProvider = Provider.of<DataProvider>(context, listen: false);
       await studentProvider.getStudents();
       await courseProvider.getCourses();
       await enrollmentProvider.getEnrollments();
@@ -60,31 +60,79 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (_) => const MainScreen()));
-                },
-                child: const Text("Go to task 1 main screen")),
-          ),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (_) => const DashboardScreen()));
-              },
-              child: const Text("Go to task 2 dashboard screen")),
-
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.blueAccent,
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (_) => const MainScreen()));
+                    },
+                    child: const Text(
+                      "Go to task 1 main screen",
+                      style: TextStyle(fontSize: 16),
+                    )),
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.blueAccent,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (_) => const DashboardScreen()));
+                  },
+                  child: const Text(
+                    "Go to task 2 dashboard screen",
+                    style: TextStyle(fontSize: 16),
+                  )),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.blueAccent,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (_) => const BmiHomePage()));
+                  },
+                  child: const Text(
+                    "BMI Calculator",
+                    style: TextStyle(fontSize: 16),
+                  )),
+            ),
+          ],
+        ),
       ),
-
     );
   }
 }
